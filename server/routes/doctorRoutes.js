4f5fcb36b11027c6ef39068c2f3dca8e7bffa901
Nisma-feather <shanthinifeathers16@gmail.com>
2025-04-19 13:43:24 +0530

    Delete server directory

--- server/routes/doctorRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { addDoctor, loginDoctor } = require("../controllers/doctorController");
-const auth = require("../middleware/authMiddleware");
-
-router.post("/add", auth("admin"), addDoctor);
-router.post("/login", loginDoctor);
-
-module.exports = router;
